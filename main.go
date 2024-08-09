d9d39e16a2d9a4cd3274f110544940b326412c16
thisantm <thiago.sant.mach@gmail.com>
2024-08-09 20:04:09 -0300

    Refactor CLI command handling and add input cleaning function

--- main.go
+++ main.go
@@ -1,36 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
-func cleanInput(input string) []string {
-	output := strings.ToLower(input)
-	readyInput := strings.Fields(output)
-	return readyInput
-}
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	cliCommandMap := getCommands()
-	for {
-		print("pokedex > ")
-		scanner.Scan()
-		input := cleanInput(scanner.Text())
-
-		if len(input) == 0 {
-			continue
-		}
-
-		command, ok := cliCommandMap[input[0]]
-		if !ok {
-			fmt.Println("Command does not exist")
-			continue
-		}
-
-		command.callback()
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 type cliCommand struct {
@@ -43,4 +45,32 @@ func commandExit() error {
 	fmt.Println("bye bye")
 	os.Exit(0)
 	return nil
+}
+
+func cleanInput(input string) []string {
+	output := strings.ToLower(input)
+	readyInput := strings.Fields(output)
+	return readyInput
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	cliCommandMap := getCommands()
+	for {
+		print("pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+
+		if len(input) == 0 {
+			continue
+		}
+
+		command, ok := cliCommandMap[input[0]]
+		if !ok {
+			fmt.Println("Command does not exist")
+			continue
+		}
+
+		command.callback()
+	}
 }
